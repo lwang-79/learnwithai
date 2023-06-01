a8df30b9df7ad0e5cf739521d8423ab27c573503
lwang <lwang79@gmail.com>
2023-06-01 19:46:47 +1000

    change app name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=optional" rel="stylesheet"></link>
         <link
             href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
             rel="stylesheet"
