7d518105fe02ab0e4a58817993b39a745d81eb09
lwang <lwang79@gmail.com>
2023-07-17 20:26:19 +1000

    bug fix

--- src/pages/math/index.tsx
+++ src/pages/math/index.tsx
@@ -69,6 +69,8 @@ function MathExam() {
   const ignoreTriggerRef = useRef(true);
 
   useEffect(() => {
+    if (!optionStatesRef.current) return;
+    
     if (optionStatesRef.current.mathSource) {
       setSelectedSource(optionStatesRef.current.mathSource);
     }
