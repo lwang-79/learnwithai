cf0a4b77e9f0ed40edb06c7137d4575da45c341a
lwang <lwang79@gmail.com>
2023-07-23 13:27:10 +1000

    miniClass

--- amplify/backend/function/learnwithaiSubscribe/src/quota.ts
+++ amplify/backend/function/learnwithaiSubscribe/src/quota.ts
@@ -5,7 +5,8 @@ export const Quota = {
     writingPerDay: 1,
     savedQuestions: 0,
     savedTests: 0,
-    savedEssays: 0
+    savedEssays: 0,
+    classroomRound: 10
   },
   personal: {
     mathPerDay: 100,
@@ -13,7 +14,8 @@ export const Quota = {
     writingPerDay: 2,
     savedQuestions: 100,
     savedTests: 50,
-    savedEssays: 50
+    savedEssays: 50,
+    classroomRound: 15
   },
   professional: {
     mathPerDay: 200,
@@ -21,7 +23,8 @@ export const Quota = {
     writingPerDay: 5,
     savedQuestions: 200,
     savedTests: 100,
-    savedEssays: 100
+    savedEssays: 100,
+    classroomRound: 30
   },
   enterprise: {
     mathPerDay: 500,
@@ -29,6 +32,7 @@ export const Quota = {
     writingPerDay: 10,
     savedQuestions: 1000,
     savedTests: 500,
-    savedEssays: 500
+    savedEssays: 500,
+    classroomRound: 50
   }
 }
